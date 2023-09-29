8ebeba5f8fc3fc50153be22314e6ee34b34a7c23
Omid-N <omid.c.nasi@gmail.com>
2023-09-28 21:34:44 -0700

    Implement environment variables on Vite and backend

--- backend/server/socketServer.ts
+++ backend/server/socketServer.ts
@@ -1,22 +1,16 @@
 
 import express from 'express';
-import { Express, Request, Response, NextFunction } from 'express';
+import { Express } from 'express';
 import bodyParser from 'body-parser';
-import gitHubRouter from '../server/routes/gitHubRouter'
-import cors from 'cors';
+import gitHubRouter from '../server/routes/gitHubRouter';
 
 const app: Express = express();
 
 app.use(bodyParser.json());
-// app.use(cors);
 
 app.use('/api/github-oauth', (req, res, next) =>{
-    console.log('hitting first route github-oauth')
-    return next();
+  console.log('hitting first route github-oauth socketServer.ts file');
+  return next();
 }, gitHubRouter);
-// app.get('/api', (req, res) => {
-//     console.log('api hit')
-//     res.status(200).send('api worked');
-// });
 
 export default app;
