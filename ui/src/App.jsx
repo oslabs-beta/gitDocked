b9528e849f890cd1f9b3e8830fc84ad012cd2823
Austin Mattus <ajmattus@gmail.com>
2023-09-30 14:17:55 -0400

    lifted stats state up to App

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import { createDockerDesktopClient } from "@docker/extension-api-client";
+import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Stack, TextField, Typography } from '@mui/material';
 import './styles.css';
 import Container from './Container';
@@ -12,42 +12,36 @@ import ContainerHealth from './ContainerHealth';
 // If you're running this React app in a browser, it won't work properly.
 // const client = createDockerDesktopClient();
 
-// function useDockerDesktopClient() {
-//   return ddClient;
-// }
-
-// new comment
-
 export function App() {
-  const ddClient = createDockerDesktopClient()
+  const ddClient = createDockerDesktopClient();
   {
     /* Use an array to store our containers initial state is empty */
   }
   const [containers, setContainers] = useState([]);
+  const [stats, setStats] = useState([]);
   {
     /* Use a boolean to check whether user is logged in or not. This will be used for conditional rendering of components*/
   }
   const [loggedIn, setLoggedIn] = useState(false);
   const [authToken, setToken] = useState('');
-  const [response, setResponse] = useState([]);
 
   const queryParams = new URLSearchParams(window.location.search);
   const code = queryParams.get('code');
 
-  async function fetchToken(){
+  async function fetchToken() {
     console.log('fetching token');
     try {
       console.log('in try block');
       const result = await ddClient.extension.vm?.service?.get(`/api/github-oauth/${code}`);
       console.log('got result');
       setToken(`${result}`);
-      console.log('this is the result', result)
+      console.log('this is the result', result);
     } catch (error) {
       console.log('this is the error', error);
     }
   }
 
-  if(!loggedIn && code){
+  if (!loggedIn && code) {
     fetchToken();
     setLoggedIn(true);
     console.log('this is the code', code);
@@ -60,45 +54,52 @@ export function App() {
   }
   async function handleButtonClick() {
     ddClient.host.openExternal('https://github.com/login/oauth/authorize?client_id=32239c9ebb7b81c40e9d');
-  };
+  }
 
   async function getStatsClick() {
-    let newData= [];
+    const newData = [];
 
     const TERMINAL_CLEAR_CODE = '\x1B[2J\x1B[H';
 
-    const result = ddClient.docker.cli.exec(
-      'stats',
-      ['--no-trunc', '--format', '{{ json . }}'],
-      {
-        stream: {
-          onOutput(data) {
-            if (data.stdout?.includes(TERMINAL_CLEAR_CODE)) {
-              // This stdout begins with the terminal clear code,
-              // meaning that it is a new sample of data.
-              setResponse(newData);
-              newData = [];
-              newData.push(
-                JSON.parse(data.stdout.replace(TERMINAL_CLEAR_CODE, ''))
-              );
-              console.log('included the terminal clear code and data', data);
-            } else {
-              console.log('else block hit', data);
-              newData.push(JSON.parse(data.stdout ?? ''));
-            }
-          },
-          onError(error) {
-            console.error(error);
-            return;
-          },
-          onClose(exitCode) {
-            console.log('docker stats exec exited with code ' + exitCode);
-            return;
-          },
-          splitOutputLines: true,
+    const result = ddClient.docker.cli.exec('stats', ['--no-trunc', '--format', '{{ json . }}'], {
+      stream: {
+        onOutput(data) {
+          console.log('on output', data);
+          if (data.stdout?.includes(TERMINAL_CLEAR_CODE)) {
+            const parseThis = data.stdout.replace(TERMINAL_CLEAR_CODE, '');
+            const parsedData = JSON.parse(parseThis);
+            newData.push(parsedData);
+          } else {
+            const entries = Object.entries(data);
+            const parse = entries[0][1];
+            const parsedDataNoTerminalCode = JSON.parse(parse);
+            newData.push(parsedDataNoTerminalCode);
+          }
+
+          setStats([...stats, ...newData]);
+
+          // display this info in our container component
+          console.log('included the terminal clear code and data', data);
+          /*
+          setStats([ ...stats, parsedData ]);
+          */
         },
-      }
-    );
+        onError(error) {
+          console.error(error);
+          return;
+        },
+        onClose(exitCode) {
+          console.log('docker stats exec exited with code ' + exitCode);
+          return;
+        },
+        splitOutputLines: true,
+      },
+    });
+
+    // stops the stats command after time passed into setTimeout
+    setTimeout(() => {
+      result.close();
+    }, 1000);
   }
 
   async function getLogsClick() {
@@ -123,112 +124,80 @@ export function App() {
   }
 
   async function getEventsClick() {
-    await ddClient.docker.cli.exec(
-      'events',
-      ['--format', '{{ json . }}', '--filter', 'container=my-container'],
-      {
-        stream: {
-          onOutput(data) {
-            if (data.stdout) {
-              const event = JSON.parse(data.stdout);
-              console.log(event);
-            } else {
-              console.log(data.stderr);
-            }
-          },
-          onClose(exitCode) {
-            console.log('onClose with exit code ' + exitCode);
-          },
-          splitOutputLines: true,
+    await ddClient.docker.cli.exec('events', ['--format', '{{ json . }}', '--filter', 'container=my-container'], {
+      stream: {
+        onOutput(data) {
+          if (data.stdout) {
+            const event = JSON.parse(data.stdout);
+            console.log(event);
+          } else {
+            console.log(data.stderr);
+          }
         },
-      }
-    );
+        onClose(exitCode) {
+          console.log('onClose with exit code ' + exitCode);
+        },
+        splitOutputLines: true,
+      },
+    });
   }
 
   // useEffect which will invoke an async function to retrieve all the user's containers and set the state equal to the array of objects
   // each object will be a container
   useEffect(() => {
     async function getContainer() {
-      console.log(await ddClient.docker.listContainers({ all: true }));
-      return await ddClient.docker.listContainers({ all: true });
+      console.log(await ddClient.docker.listContainers());
+      return await ddClient.docker.listContainers();
     }
 
     getContainer().then((allContainers) => {
+      console.log('all containers', allContainers);
       setContainers(allContainers);
     });
+
+    getStatsClick();
   }, []);
 
   return (
     <>
-      <body className="body">
-        <h1 className="test">Welcome to your dashboard!!!</h1>
+      <div className='body'>
+        <h1 className='test'>Welcome to your dashboard</h1>
         <button onClick={handleButtonClick}>Log in through Github</button>
         <button onClick={getStatsClick}>Get Docker Stats</button>
         <button onClick={getLogsClick}>Get Docker Logs</button>
         <button onClick={getEventsClick}>Get Docker Events</button>
-        <div className="box">
-          <div className="container-grid">
+        <div className='box'>
+          <div className='container-grid'>
             {/*Containers go here*/}
             {containers.map((container, index) => {
-              return <Container key={index} details={container} />;
+              return <Container key={index} details={container} stats={stats} />;
             })}
           </div>
 
-          <div className="log-grid">
+          <div className='log-grid'>
             {/*Log goes here*/}
             {loggedIn && <StatusLog />}
             <h1>Not logged in!</h1>
           </div>
         </div>
 
-        <div className="container-health">
+        <div className='container-health'>
           {/* Container Health Comparison goes here */}
           <ContainerHealth />
         </div>
-      </body>
+      </div>
     </>
   );
 }
 
-{
-  /*
-  const [response, setResponse] = React.useState<string>();
-  const ddClient = useDockerDesktopClient();
-
-  const fetchAndDisplayResponse = async () => {
-    console.log('trying to fetch /api')
-    const result = await ddClient.extension.vm?.service?.get('/api');
-    console.log('result: ', result);
-    setResponse(JSON.stringify(result));
-  };
-
-  return (
-    <>
-      <Typography variant="h3">GitDocked</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        This is a basic page rendered with MUI, using Docker's theme. Read the
-        MUI documentation to learn more. Using MUI in a conventional way and
-        avoiding custom styling will help make sure your extension continues to
-        look great as Docker's theme evolves.
-      </Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        Pressing the below button will trigger a request to the backend. Its
-        response will appear in the textarea.
-      </Typography>
-      <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={fetchAndDisplayResponse}>
-          Call backend
-        </Button>
-
-        <TextField
-          label="Backend response"
-          sx={{ width: 480 }}
-          disabled
-          multiline
-          variant="outlined"
-          minRows={5}
-          value={response ?? ''}
-        />
-  </Stack>
-  */
-}
+// {
+//   "stdout": "\u001b[2J\u001b[H{\"BlockIO\":\"68.3MB / 4.1kB\",
+//   \"CPUPerc\":\"0.56%\",
+//   \"Container\":\"cd70a1ef811a\",
+//   \"ID\":\"cd70a1ef811ad951181ed7299150bf4274b06b961c441a809a985ef09eb01a54\",
+//   \"MemPerc\":\"0.63%\",
+//   \"MemUsage\":\"48.67MiB / 7.579GiB\",
+//   \"Name\":\"gitdocked\",
+//   \"NetIO\":\"15.5kB / 2.97kB\",
+//   \"PIDs\":\"22\"}"
+// }
--- ui/src/Container.jsx
+++ ui/src/Container.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 
 // this component shows a tile for each of the user's containers
 // this includes active and inactive containers
-export default function Container({ details }) {
+export default function Container({ details, stats }) {
   // destructure the details prop which is an object holding information on the container
 
   // declaring variables for id and name which we will grab from the container object
   const id = details.Id;
   const name = details.Names;
+  // const cpu = stats[0].CPUPerc;
+  // const memUsage = stats[0].MemUsage;
+  // const netIO = stats.NetIO;
 
   return (
-    <div className="container">
+    <div className='container'>
       {/* Invoke slice method on id so that we have the shorter version of the ID which fits inside the size of the component */}
       <h4>Container ID: {id.slice(-10)}</h4>
       <h5>Container_Name: {name}</h5>
       {/* Still have to grab info for the below header tags*/}
-      <h5>CPU</h5>
-      <h5>Memory</h5>
-      <h5>Network I/O</h5>
+      <h5>CPU:</h5>
+      <h5>Memory Usage:</h5>
+      <h5>Network I/O:</h5>
 
       {/* Still have to add functionality for these buttons */}
-      <button className="small-button">Start</button>
-      <button className="small-button">Pause</button>
-      <button className="small-button">Restart</button>
-      <button className="small-button">Delete</button>
+      <button className='small-button'>Start</button>
+      <button className='small-button'>Pause</button>
+      <button className='small-button'>Restart</button>
+      <button className='small-button'>Delete</button>
     </div>
   );
 }
