35e3547807b66c0934d225524a94884faa9c8712
Austin Mattus <ajmattus@gmail.com>
2023-11-02 13:13:27 -0400

    added sample line chart

--- ui/src/Charts.jsx
+++ ui/src/Charts.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Legend, Tooltip } from 'chart.js';
+
+ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Legend, Tooltip);
 
 export default function Charts() {
+  const data = {
+    labels: ['Mon', 'Tue', 'Wed'],
+    datasets: [
+      {
+        label: 'Sales of the Week',
+        data: [6, 3, 9],
+        backgroundColor: 'aqua',
+        borderColor: 'black',
+        pointBorderColor: 'aqua',
+        tension: 0.4,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: true,
+    },
+    scales: {
+      y: {
+        min: 3,
+        max: 6,
+      },
+    },
+  };
+
   return (
     <>
-      <div>
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-        </ul>
+      <div style={{width: '600px', height: '300px', padding: '20px'}}>
+        <Line data={data} options={options}></Line>
       </div>
     </>
   );
